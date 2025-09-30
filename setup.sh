85dfefb305f4f87093e4091b716e606d134d5813
Mark Zhou <xinyunzhouu@gmail.com>
2025-09-29 23:26:35 -0400

    Use venv

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+# Create virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Download COCO datasets
@@ -13,4 +18,5 @@ unzip -n train2017.zip
 unzip -n val2017.zip
 cd ..
 
-echo "Setup complete! Run: python train.py"
+echo "Setup complete! Activate venv with: source venv/bin/activate"
+echo "Then run: python train.py"
