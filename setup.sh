3b40c31abaadb7c42db4721db05f5e2633bf1b62
Mark Zhou <xinyunzhouu@gmail.com>
2025-09-30 08:48:23 -0400

    Don't print for every unzipped file

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ mkdir -p data
 cd data
 wget -nc http://images.cocodataset.org/zips/train2017.zip
 wget -nc http://images.cocodataset.org/zips/val2017.zip
-unzip -n train2017.zip
-unzip -n val2017.zip
+unzip -q -n train2017.zip
+unzip -q -n val2017.zip
 cd ..
 
 echo "Setup complete! Activate venv with: source venv/bin/activate"
