c171daf1a6d7c74d45cc6634068603eb219ced35
Mark Zhou <xinyunzhouu@gmail.com>
2025-09-29 21:53:12 -0400

    Bugfixes

--- src/__init__.py
+++ src/__init__.py
@@ -2,16 +2,15 @@
 
 __version__ = "0.1.0"
 
-from .dataset import PreprocessedRotationDataset, create_dataloaders
-from .trainer import Trainer, ModelWrapper
+from .dataset import RotationDataset, create_dataloaders
+from .trainer import Trainer
 from .exporter import export_model
 from .downloader import download_and_extract, verify_dataset
 
 __all__ = [
-    "PreprocessedRotationDataset",
+    "RotationDataset",
     "create_dataloaders",
     "Trainer",
-    "ModelWrapper",
     "export_model",
     "download_and_extract",
     "verify_dataset",
