1ba7b2fe70902e2d8b9c88b7b51acda276d7115e
Mark Zhou <xinyunzhouu@gmail.com>
2025-10-02 23:51:10 -0400

    Use medium model

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ def main():
     print(f"Training on {len(train_dataset)} images, validating on {len(val_dataset)} images")
 
     # Model
-    model = timm.create_model("mobilenetv4_conv_small.e2400_r224_in1k", pretrained=True, num_classes=4)
+    model = timm.create_model("mobilenetv4_conv_medium.e500_r256_in1k", pretrained=True, num_classes=4)
     model = model.to(device)
 
     # Training setup
